8d8c132c6de3800e98403e37ef7e3e494dcd8258
Beerendan <brendanrbowen@gmail.com>
2023-05-25 19:46:38 -0400

    still not working idk why

--- config/config.js
+++ config/config.js
@@ -1,15 +1,16 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-  }
-);
-
-module.exports = sequelize;
+module.exports = {
+    HOST: process.env.HOST,
+    USER: 'root',
+    PASSWORD: process.env.PASSWORD,
+    DB: "recipe_db",
+    dialect: "mysql",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
